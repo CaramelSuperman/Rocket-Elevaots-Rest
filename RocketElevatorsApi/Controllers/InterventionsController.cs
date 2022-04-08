c6ce1452e624c99c3b2444ea849298a59c9daae2
CaramelSuperman <98334952+CaramelSuperman@users.noreply.github.com>
2022-04-08 12:09:04 -0400

    for david

---
+++ RocketElevatorsApi/Controllers/InterventionsController.cs
@@ -0,0 +1,82 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RocketElevatorsApi.Data;
+using RocketElevatorsApi.Models;
+
+namespace RocketElevatorsApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InterventionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public InterventionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+         // GET: api/elevators/status
+        [HttpGet("status")]
+        public async Task<ActionResult<IEnumerable<Intervention>>> GetInterventionsStatus()
+        {
+            await _context.interventions.ToListAsync();
+            List<Intervention> interventions = await _context.interventions.ToListAsync();
+            List<Intervention> pendingInterventions = new List<Intervention>();
+           
+           foreach (Intervention b in interventions)
+           {
+              if (b.status == "Pending")
+                {
+                    pendingInterventions.Add(b);
+                }
+
+           
+            } 
+            return pendingInterventions;
+        }
+
+        // POST: api/elevators/Status/1?status=offline
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("Changestatus1/{id}")]
+        public async Task<ActionResult<Intervention>> ChangeInterventionStatusToInProgress(long id)
+        {
+
+            var intervention = await _context.interventions.FindAsync(id);
+            if (intervention.status == "Pending")
+                       
+            intervention.status = "InProgress";
+            intervention.start_intervention = DateTime.Now;
+            await _context.SaveChangesAsync();
+             
+
+             return intervention;
+        }
+
+         // GET: api/elevators/offline
+         [HttpPut("Changestatus2/{id}")]
+        public async Task<ActionResult<Intervention>> ChangeInterventionStatusToCompleted(long id)
+        {
+
+            var intervention = await _context.interventions.FindAsync(id);
+            if (intervention.status == "InProgess")
+                       
+            intervention.status = "Completed";
+            intervention.end_intervention = DateTime.Now;
+            await _context.SaveChangesAsync();
+            
+            return intervention;
+        }
+
+        private bool ElevatorExists(long id)
+        {
+            return _context.elevators.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ RocketElevatorsApi/Models/Intervention.cs
@@ -0,0 +1,17 @@
+public class Intervention{
+    public long Id { get; set; }
+    public string? author { get; set; }
+    public string? customerID {get; set;}
+    public string? buildingD  {get; set;}
+    public string? batteryID {get; set;}
+    public string? columnID {get; set;}
+     public string? elevatorID {get; set;}
+    public DateTime? start_intervention{get; set;}
+    public DateTime? end_intervention {get; set;}
+
+    public string? result {get; set;}
+    public string? report {get; set;}
+    public string? status {get; set;}
+    public string? employee {get; set;}
+
+}
